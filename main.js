d1c865a440b47680fe544d2d84d62301ba363bfd
Matt Hensley <matt.hensley777@gmail.com>
2017-07-31 19:03:23 -0500

    cat

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var cat = "fluffy";
 console.log("hello");
 console.log("fixed");
+
+
+var catzContainer = document.getElementById("catz");
+
+console.log(catzContainer);
+catzContainer.innerHTML = '<h1>' + cat + '</h1>';
